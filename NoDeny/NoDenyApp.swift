23023aa6a3534282880220ba92bd0205d52322e3
Evgeniy Podyachev <zeny@yugit.ru>
2023-04-19 16:16:59 +0300

    Initial Commit

---
+++ NoDeny/NoDenyApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoDenyApp.swift
+//  NoDeny
+//
+//  Created by Евгений Подъячев on 19.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct NoDenyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
